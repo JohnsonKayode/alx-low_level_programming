f3274d33d71fb755bb826caeed508fbe5c903d8a
JohnsonKayode <kayodejohnson01@gmail.com>
2022-09-08 15:04:35 -0700

    number 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints on screen
+ *
+ * Description: this prints the text below
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0)
+}
