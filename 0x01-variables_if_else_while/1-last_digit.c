809fa91592d45cfb86ee8aa93cc520bc37693d07
JohnsonKayode <kayodejohnson01@gmail.com>
2022-09-12 02:00:00 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	} else if (1 < 6 && 1 != 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
