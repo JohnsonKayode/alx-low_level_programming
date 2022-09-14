7b3e35f4ab357867405f71f85c0c40d533f76d95
JohnsonKayode <kayodejohnson01@gmail.com>
2022-09-14 13:33:42 -0700

    print lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - print the alphabet in lowercase.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
@@ -15,6 +15,4 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
-
 }
