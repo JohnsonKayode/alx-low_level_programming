e7ea4a342628b807ccc8b3403379dbaaa9f2a76c
JohnsonKayode <kayodejohnson01@gmail.com>
2022-09-20 12:45:40 -0700

    The main.h and the first question

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this resets the value n of int to 98
+ *
+ * @n: a pointer to an int variable type
+ *
+ * Return: return is void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
