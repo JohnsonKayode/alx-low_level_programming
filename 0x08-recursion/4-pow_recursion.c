edf89edda3e4881e40ed8e365d756aa742fc9ff6
JohnsonKayode <kayodejohnson01@gmail.com>
2022-09-29 06:13:29 -0700

    gives the value of an int raised to the power of another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - gives the value of an int
+ * rised to the power of another
+ * @x: base int
+ * @y: exponent
+ *
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int power;
+
+	if (y == 0)
+		power = (1);
+	else if (y < 0)
+		power = (-1);
+	else if (y > 0)
+	{
+		power = (x * _pow_recursion(x, (y - 1)));
+	}
+	return (power);
+}
