0397140d78006a3b99daf398a26130b85bfb9932
JohnsonKayode <kayodejohnson01@gmail.com>
2022-10-02 14:37:52 -0700

    print the arguement passed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name followed by new line
+ * @argc: arcuement counter
+ * @argv: arguement container
+ *
+ * Return: String i array
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argc[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is for the arguements contents and vector
