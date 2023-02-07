596432b2daa9abdcb0e2d9b2d1d7247f848e5e2f
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-07 19:04:21 +0000

    readt xt

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,45 @@
 #include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) return 0;
-    
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) 
-	return 0;
-	
-	ssize_t printout = write(STDOUT_FILENO, file, letters);
-	if (write == NULL)
-		return 0;
-	if (letter == NULL)
-		return 0;
+/*
+* read_textfile: reads a file and prints output
+* filename: the name of file
+* c: The output or printout holder
+* temp: temporary place holder for letters and 
+* keeps count of number of lines to be printed
+* readable: Hold number of letters in file
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+		exit(1);
+	}
+	char c;
+	int readable;
+	FILE *file = fopen(filename, "r");
+	if (file == NULL)
 
-	_putchar(printout);
+	{
+		return (0);
+		exit(1);
+	}
+	size_t *temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
+	{
+		return (0);
+		exit(1);
+	}
+	else
+	{
+		readable = read(STDIN_FILENO, file, temp);
+		while (!feof(file))
+		{
+			c = write(STDOUT_FILENO, file, temp);
+			_putchar(c);
+		}
+		free(temp);
+	}
 	fclose(file);
-	return printout;
+	return (readable);
 }
-
