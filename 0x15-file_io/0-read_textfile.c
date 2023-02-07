a438c1a3394bec3c53ec3cd198e06c01483b169a
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-07 20:36:25 +0000

    text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,37 @@
 #include "main.h"
 
-/*
-* read_textfile: reads a file and prints output
-* filename: the name of file
-* c: The output or printout holder
-* temp: temporary place holder for letters and 
-* keeps count of number of lines to be printed
-* readable: Hold number of letters in file
-*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int file;
+	ssize_t readable, written;
+	char buffer;
+
 	if (filename == NULL)
 	{
 		return (0);
-		exit(1);
 	}
-	char c;
-	int readable;
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
 
+	file = open(filename, O_RDONLY);
+	if (file == NULL)
 	{
 		return (0);
-		exit(1);
 	}
-	size_t *temp = malloc(sizeof(char) * letters);
-	if (temp == NULL)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 	{
 		return (0);
-		exit(1);
 	}
-	else
+	readable = read(file, buffer, letters);
+	if (readable == NULL)
 	{
-		readable = read(STDIN_FILENO, file, temp);
-		while (!feof(file))
-		{
-			c = write(STDOUT_FILENO, file, temp);
-			_putchar(c);
-		}
-		free(temp);
+		free(buffer);
+		return (0);
 	}
-	fclose(file);
+	written = write(STDOUT_FILENO, buffer, readable);
+	free(buffer);
+	if (writter != readable)
+	{
+		return (0);
+	}
 	return (readable);
-}
+
