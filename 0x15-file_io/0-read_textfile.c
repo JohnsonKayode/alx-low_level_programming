4fb3810b1fb5b6065af2f278441f6a2de3828328
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-07 15:50:18 +0000

    read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,20 @@
-
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters) {
     if (filename == NULL) return 0;
     
     FILE *file = fopen(filename, "r");
-    if (file == NULL) return 0;
-    
-    char *buffer = (char *) malloc(sizeof(char) * letters);
-    if (buffer == NULL) return 0;
-    
-    size_t read_bytes = fread(buffer, sizeof(char), letters, file);
-    if (read_bytes < letters) {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
-    
-    ssize_t written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-    if (written_bytes < read_bytes) {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
-    
-    free(buffer);
-    fclose(file);
-    return written_bytes;
+    if (file == NULL) 
+	return 0;
+	
+	ssize_t printout = write(STDOUT_FILENO, file, letters);
+	if (write == NULL)
+		return 0;
+	if (letter == NULL)
+		return 0;
+
+	_putchar(printout);
+	fclose(file);
+	return printout;
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
