a396c38bc4ffa050815671acd8b620e70058318b
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-09 18:44:14 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
-int create_file(const char *filename, char *text_content) {
-    int fileDescriptor;
-    mode_t filePermissions = S_IRUSR | S_IWUSR;
+/**
+  * create_file - Create a function that creates a file.
+  *
+  * @filename: The file name to be printed
+  * @text_content: Numbers of letter to be read.
+  *
+  * Return: Always (0)
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fileDescriptor = open(filename, O_CREAT | O_TRUNC | O_WRONLY, filePermissions);
-	if (fileDescriptor == -1)
-	{
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-    if (text_content != NULL) {
-        if (write(fileDescriptor, text_content, strlen(text_content)) == -1) {
-            return (-1);
-        }
-    }
+	close(o);
 
-    if (close(fileDescriptor) == -1)
-		return (-1);
-    return (1);
+	return (1);
 }
-
